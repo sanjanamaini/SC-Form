5684e19dcf2a006fa722da631be99e2b937ee18f
Sanjana <sanjumaini18@gmail.com>
2023-06-22 09:51:55 +0530

    new

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get a reference to the form
+    var form = document.querySelector('form');
+
+    // Attach a submit event listener to the form
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the form from submitting
+
+        // Display a success message
+        var message = document.createElement('p');
+        message.textContent = 'Thank you for submitting the form!';
+        message.style.color = 'green';
+        form.appendChild(message);
+    });
+});
